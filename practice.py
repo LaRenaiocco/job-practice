f9f37979151605d45dc8fc371c205a376740d0aa
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-09-30 00:05:24 -0400

    concat_lists

--- practice.py
+++ practice.py
@@ -1,16 +1,33 @@
-#Given list of ints, return True if any two nums in list sum to 0.
+# Given two lists. concatenate them (that is, combine them into a single list).
+
+def concat_lists(list1, list2):
+    """Combine lists.
+    >>> concat_lists([1, 2], [3, 4])
+    [1, 2, 3, 4]
+    >>> concat_lists([], [1, 2])
+    [1, 2]
+    >>> concat_lists([1, 2], [])
+    [1, 2]
+    >>> concat_lists([], [])
+    []
+    """
+
+    result = list1
+    for item in list2:
+        result.append(item)
+    return result
+
+
+# Given list of ints, return True if any two nums in list sum to 0.
 
 def add_to_zero(nums):
     """Given list of ints, return True if any two nums sum to 0.
     >>> add_to_zero([])
     False
-
     >>> add_to_zero([1])
     False
-
     >>> add_to_zero([1, 2, 3])
     False
-
     >>> add_to_zero([1, 2, 3, -2])
     True
 """
