ac7893991e079aa2c8d98181bf96c7a8287cee96
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-09-30 15:08:56 -0400

    subtractProductAndSum

--- practice.py
+++ practice.py
@@ -1,3 +1,38 @@
+# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+def subtractProductAndSum(self, n):
+    """ Subtract sum from product of digits on an integer"""
+    # convert integer to string and split into a list
+    str_list = list(str(n))
+    # create sum variable
+    sumation = 0
+    # create product variable
+    product = 1
+    # iterate through list
+    for char in str_list:
+        num = int(char)
+        # convert each item to int and add to sum total
+        sumation += num
+        # convert each item to int and multiply to product total
+        product *= num
+    # return product minus sum
+    return product - sumation
+
+# Given an integer n, return a list containing n unique random numbers between 1-10, inclusive.
+
+# def lucky_numbers(n):
+#     """Return n unique random numbers from 1-10 (inclusive)."""
+
+#     from random import randint
+
+#     lucky_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     result = set([])
+#     for num in range(1, n + 1):
+#         lucky = randint(lucky_nums)
+#         if lucky not in result:
+#             result.append(lucky)
+#         else
+
 # Given two lists. concatenate them (that is, combine them into a single list).
 
 def concat_lists(list1, list2):
