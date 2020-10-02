5977167bc785ae74d1b83b983eb1c25efcf866d3
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-10-02 15:43:45 -0400

    compress string and has balanced brackets solved.

--- practice.py
+++ practice.py
@@ -1,3 +1,106 @@
+"""Write a function that compresses a string.
+
+Repeated characters should be compressed to one character and the number of
+times it repeats:
+
+>>> compress('aabbaabb')
+'a2b2a2b2'
+
+If a character appears once, it should not be followed by a number:
+
+>>> compress('abc')
+'abc'
+
+The function should handle letters, whitespace, and punctuation:
+
+>>> compress('Hello, world! Cows go moooo...')
+'Hel2o, world! Cows go mo4.3'
+"""
+
+
+def compress(string):
+    """Return a compressed version of the given string."""
+    char_list = []
+    for char in string:
+        char_list.append(char)
+    new_list = [char_list[0]]
+    index = 0
+    count = 1
+    while index < len(char_list)-1:
+        if char_list[index] == char_list[index + 1]:
+            count += 1
+        else:
+            if count != 1:
+                new_list.append(str(count))
+                count = 1
+            new_list.append(char_list[index + 1])
+        index += 1
+    if count != 1:
+        new_list.append(str(count))
+    new_string = "".join(new_list)
+    return new_string
+
+## I don't like this solution!!! Need to attempt again with a stack!
+
+def has_balanced_brackets(phrase):
+    """Does a given string have balanced pairs of brackets?
+
+    Given a string as input, return True or False depending on whether the
+    string contains balanced (), {}, [], and/or <>.
+    >>> has_balanced_brackets("<ok>")
+    True
+    >>> has_balanced_brackets("<[ok]>")
+    True
+    >>> has_balanced_brackets("<[{(yay)}]>")
+    True
+    >>> has_balanced_brackets("(Oops!){")
+    False
+    >>> has_balanced_brackets("{[[This has too many open square brackets.]}")
+    False
+    >>> has_balanced_brackets(">")
+    False
+    >>> has_balanced_brackets("(This has {too many} ) closers. )")
+    False
+    >>> has_balanced_brackets("<{Not Ok>}")
+    False
+    >>> has_balanced_brackets("No brackets here!")
+    True
+    """
+
+    # Keep a count of each type of bracket and keep a list of order
+
+    open_bracket_types = {"(", "{", "[", "<"}
+    close_bracket_types = {")", "}", "]", ">"}
+    bracket_order = []
+    for char in phrase:
+        if char in open_bracket_types:
+            bracket_order.append(char)
+        if char in close_bracket_types:
+            if bracket_order == []:
+                return False
+            else:
+                match = bracket_order.pop()
+            if char == ")":
+                if match != "(":
+                    return False
+            if char == "}":
+                if match != "{":
+                    return False
+            if char == "]":
+                if match != "[":
+                    return False
+            if char == ">":
+                if match != "<":
+                    return False
+    if bracket_order == []:
+        return True
+    else:
+        return False                
+
+
+
+# Check if a phrase has balanced parentheses
+
 def has_balanced_parens(phrase):
     """Does a string have balanced parentheses?
     >>> has_balanced_parens("()")
@@ -199,4 +302,5 @@ def add_to_zero(nums):
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
