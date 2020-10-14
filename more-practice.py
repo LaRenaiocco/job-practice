a4f8d4790d6654f772f78ab8e5787e908d6a3388
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-10-13 21:01:48 -0400

    ROTL13 solved (codewars)

--- more-practice.py
+++ more-practice.py
@@ -1,3 +1,24 @@
+def rot13(message):
+    alpha = {
+        "a": "n", "A": "N", "b": "o", "B": "O", "c": "p", "C": "P", 
+        "d": "q", "D": "Q", "e": "r", "E": "R", "f": "s", "F": "S",
+        "g": "t", "G": "T", "h": "u", "H": "U", "i": "v", "I": "V",
+        "j": "w", "J": "W", "k": "x", "K": "X", "l": "y", "L": "Y",
+        "m": "z", "M": "Z", "n": "a", "N": "A", "o": "b", "O": "B",
+        "p": "c", "P": "C", "q": "d", "Q": "D", "r": "e", "R": "E",
+        "s": "f", "S": "F", "t": "g", "T": "G", "u": "h", "U": "H",
+        "v": "i", "V": "I", "w": "j", "W": "J", "x": "k", "X": "K",
+        "y": "l", "Y": "L", "z": "m", "Z": "M"
+        }
+    result = ""
+    for char in message:
+        new = alpha.get(char, None)
+        if new == None:
+            result += char
+        else: 
+            result += new
+    return result
+
 def areYouPlayingBanjo(name):
     if name[0] == "R" or name[0] == "r":
         return f"{name} plays banjo"
