7874eba6dc34348b35d751c047906ae3d13b15e1
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-10-21 19:18:52 -0400

    Dubstep solved (codewars)

--- codewars.py
+++ codewars.py
@@ -1,6 +1,17 @@
+#Dubstep (6 kyu)
+def song_decoder(song):
+    words = song.split("WUB")
+    og_song = ""
+    for word in words:
+        if word != "":
+            og_song += word
+            og_song += " "
+
+    return og_song.strip(" ")
+
 # Remove the Parentheses (6 kyu)
 def remove_parentheses(s):
-    
+
     parens = 0
     result = ""
     for char in s:
