95ce6193916ee9b5e21c456b16fd60f97452ace5
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-11-24 16:41:52 -0500

    Arranging coins solved (leetcode)

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,37 @@
+#arranging coins #441
+def arrangeCoins(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    rows = 0
+    while rows < n:
+        n -= rows
+        if rows < n:
+            rows += 1
+
+    return rows
+
 # Assign Cookies #455
-    def findContentChildren(g, s):
-        """
-        :type g: List[int]
-        :type s: List[int]
-        :rtype: int
-        """
-        count = 0
-        g.sort()
-        s.sort()
-        if len(s) == 0 or len(g) == 0:
-            return 0
-        while len(g) > 0 and len(s) > 0:
-            if g[-1] <= s[-1]:
-                count += 1
-                g.pop()
-                s.pop()
-            elif g[-1] > s[-1]:
-                g.pop()
-        return count
+def findContentChildren(g, s):
+    """
+    :type g: List[int]
+    :type s: List[int]
+    :rtype: int
+    """
+    count = 0
+    g.sort()
+    s.sort()
+    if len(s) == 0 or len(g) == 0:
+        return 0
+    while len(g) > 0 and len(s) > 0:
+        if g[-1] <= s[-1]:
+            count += 1
+            g.pop()
+            s.pop()
+        elif g[-1] > s[-1]:
+            g.pop()
+    return count
 
 #258: Add digits
 
