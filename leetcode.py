1a1fd414954f754e2776da1eb63340c4441b3821
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-11-24 16:26:19 -0500

    Assign Cookies solved (leetcode)

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,24 @@
+# Assign Cookies #455
+    def findContentChildren(g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        count = 0
+        g.sort()
+        s.sort()
+        if len(s) == 0 or len(g) == 0:
+            return 0
+        while len(g) > 0 and len(s) > 0:
+            if g[-1] <= s[-1]:
+                count += 1
+                g.pop()
+                s.pop()
+            elif g[-1] > s[-1]:
+                g.pop()
+        return count
+
 #258: Add digits
 
 def addDigits(num):
