0b2aa9f6cb88f71256e8d6f0c18bf52d752f6a83
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-12-03 19:13:46 -0500

    Balanced parens solved (codewars)

--- codewars.py
+++ codewars.py
@@ -1,3 +1,19 @@
+#chack for balanced parens (5kyu)
+def valid_parentheses(string):
+    open_count = 0
+    for char in string:
+        if char == '(':
+            open_count += 1
+        elif char == ')':
+            if open_count == 0:
+                return False
+            else:
+                open_count -= 1
+    if open_count == 0:
+        return True
+    else:
+        return False
+
 #return array of nums from n to 1
 def reverse_seq(n):
     result = []
