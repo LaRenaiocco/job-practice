131d85bf5efc0f80b48a7b7401bf45e1cd547224
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-12-10 19:04:50 -0500

    Which is Larger - javascript solved

---
+++ javascriptPractice.js
@@ -0,0 +1,22 @@
+// return the min and max of an array as an array [min, max]
+
+function minMax(arr) {
+	const minNum = Math.min(...arr);
+	const maxNum = Math.max(...arr);
+	const result = [minNum, maxNum];
+	return result;
+}
+
+//  f and g are functions that take no arguments and return an integer
+//  find with returns a larger integer and return 'f', 'g' or 'neither'
+function whichIsLarger(f, g) {
+	const fResult = f()
+	const gResult = g()
+	if (fResult > gResult) {
+		return 'f'
+	} else if ( gResult > fResult) {
+		return 'g'
+	} else {
+		return 'neither'
+	}
+}
