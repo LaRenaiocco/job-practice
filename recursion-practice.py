4b8541ea9d199d17ad12c92d8ecc16677cd47aa4
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-12-15 16:06:54 -0500

    build binary search tree with recursion

--- recursion-practice.py
+++ recursion-practice.py
@@ -8,9 +8,9 @@ def flatten(my_list):
     result = []
     for item in my_list:
         if isinstance(item, list):
-        print("List found!")
-        flat_list = flatten(item)
-        result.extend(flat_list)
+            print("List found!")
+            flat_list = flatten(item)
+            result.extend(flat_list)
     else:
         result.append(item)
     
@@ -21,8 +21,21 @@ planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uran
 
 #fibonacci sequence with recursion
 def fibonacci(n):
-  if n == 1:
-    return 1
-  if n == 0:
-    return 0
-  return fibonacci(n - 1) + fibonacci(n - 2)
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+# build a binary earch tree with recursion from a sorted list
+def build_bst(my_list):
+    if my_list == []:
+        return "No Child"
+    middle_idx = len(my_list) // 2
+    middle_value = my_list[middle_idx]
+    print(f"Middle index: {middle_idx}")
+    print(f"Middle value: {middle_value}")
+    tree_node = {"data": middle_value}
+    tree_node["left_child"] = build_bst(my_list[:middle_idx])
+    tree_node["right_child"] = build_bst(my_list[middle_idx + 1:])
+        return tree_node
