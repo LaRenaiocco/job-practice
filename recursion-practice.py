ca1575deed52c0c4fccd1e6c0f550a77648969b7
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-12-15 15:20:45 -0500

    fibonacci sequence with recursion

--- recursion-practice.py
+++ recursion-practice.py
@@ -4,7 +4,6 @@ def sum_to_one(n):
     print("Recursing with input: {0}".format(n))
     return n + sum_to_one(n - 1)
 
-# define flatten() below...
 def flatten(my_list):
     result = []
     for item in my_list:
@@ -19,3 +18,11 @@ def flatten(my_list):
 
 ### reserve for testing...
 planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uranus', ['neptune', 'pluto']]
+
+#fibonacci sequence with recursion
+def fibonacci(n):
+  if n == 1:
+    return 1
+  if n == 0:
+    return 0
+  return fibonacci(n - 1) + fibonacci(n - 2)
