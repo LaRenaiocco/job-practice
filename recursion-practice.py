1c3f6a6dfbe5c044a36f75b293b145d9d7424f35
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-12-15 15:03:16 -0500

    flatten nested lists with recursion

--- recursion-practice.py
+++ recursion-practice.py
@@ -1,5 +1,21 @@
 def sum_to_one(n):
-  if n == 1:
-    return n
-  print("Recursing with input: {0}".format(n))
-  return n + sum_to_one(n - 1)
+    if n == 1:
+        return n
+    print("Recursing with input: {0}".format(n))
+    return n + sum_to_one(n - 1)
+
+# define flatten() below...
+def flatten(my_list):
+    result = []
+    for item in my_list:
+        if isinstance(item, list):
+        print("List found!")
+        flat_list = flatten(item)
+        result.extend(flat_list)
+    else:
+        result.append(item)
+    
+    return result
+
+### reserve for testing...
+planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uranus', ['neptune', 'pluto']]
