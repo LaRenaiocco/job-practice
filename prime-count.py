0f5799a73600c9220d5cdecc1a648dacd54cec39
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-01-19 23:07:18 -0500

    Prime Count solved

--- prime-count.py
+++ prime-count.py
@@ -12,3 +12,34 @@ def primes(count):
     >>> primes(5)
     [2, 3, 5, 7, 11]
     """
+
+    if count == 0:
+        return []
+    if count == 1:
+        return [2]
+    current = 3
+    result = [2]
+    while len(result) < count:
+        prime = True
+        if current % 2 == 0:
+            prime = False
+        else:
+            half_curr = (current // 2) + 1
+            for num in range(2, half_curr):
+                if current % num == 0:
+                    prime = False
+            if prime == True:
+                result.append(current)
+        current += 1
+
+    return result
+
+
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
