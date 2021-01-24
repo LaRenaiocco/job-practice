cf1f87897605dcb6af6ed28524754e97c38fdb38
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-01-23 22:39:12 -0500

    Solved Negative numbers - Codewars

---
+++ negative-nums.py
@@ -0,0 +1,10 @@
+# #CODEWARS
+# return the negative number of input.
+# 0 returns 0
+# negatives are already negative
+
+def make_negative( number ):
+    if number < 1:
+        return number
+    else:
+        return number * -1
