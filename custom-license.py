e384712b4950f72553b618bcd9b0bbfee8e4842c
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-01-31 17:41:16 -0500

    Custom License Plate solved - Codewars 6kyu

--- custom-license.py
+++ custom-license.py
@@ -29,8 +29,69 @@
 # "1337"        -->  "not possible"
 
 def licence_plate(s):
-    # your brilliant code here
-    pass
+    """
+    >>> licence_plate("mercedes")
+    'MERCEDES'
+    >>> licence_plate("anter69")
+    'ANTER-69'
+    >>> licence_plate('1st')
+    '1-ST'
+    >>> licence_plate("~c0d3w4rs~")
+    'C-0-D-3'
+    >>> licence_plate("I'm cool!")
+    'I-M-COOL'
+    >>> licence_plate("1337")
+    'not possible'
+    """
+    if s.isnumeric() or len(s) < 2:
+        return 'not possible'
+    
+    result = ''
+    previous = None
+    
+    for char in s:
+        if char.isalpha():
+            if len(result) == 0:
+                result += char.upper()
+                previous = char
+            else:
+                if previous.isalpha():
+                    result += char.upper()
+                    previous = char
+                elif previous.isnumeric():
+                    result += f'-{char.upper()}'
+                    previous = char
+                else:
+                    result += char.upper()
+                    previous = char
+        elif char.isnumeric():
+            if len(result) == 0:
+                result += char
+                previous = char
+            else:
+                if previous.isnumeric():
+                    result += char
+                    previous = char
+                elif previous.isalpha():
+                    result += f'-{char}'
+                    previous = char
+                else:
+                    result += char
+                    previous = char
+        else:                               # char must be a special char
+            if len(result) == 0 or previous == '-':
+                pass
+            else:
+                result += '-'
+                previous = '-'
+
+    result = result[0:8]
+    while result[-1] == '-':
+        result = result[0:-1]
+    if result.isnumeric() or len(result) <2:
+        return 'not possible'
+    else:
+        return result
 
 # Task
 # return plate with max of 8 characters or "not possible"
@@ -40,6 +101,7 @@ def licence_plate(s):
 # dash between letters and numbers
 
 # pseudocode:
+#quick break for all numeric
 # plate result variable
 # variable for previous character tracking for easy comparison
 # iterate through input, while result is less than 8
