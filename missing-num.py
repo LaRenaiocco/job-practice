2e4d73b58479b90afbff889d31f911683ecc8ca2
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-03-03 22:02:17 -0800

    Leetcode March Challenge day 3 solved

---
+++ missing-num.py
@@ -0,0 +1,48 @@
+# Given an array nums containing n distinct numbers in the range [0, n], 
+# return the only number in the range that is missing from the array.
+
+# Follow up: Could you implement a solution using only O(1) extra space 
+# complexity and O(n) runtime complexity?
+
+def missing_num(nums):
+    """
+    >>> missing_num([3,0,1])
+    2
+    >>> missing_num([0,1])
+    2
+    >>> missing_num([9,6,4,2,3,5,7,0,1])
+    8
+    >>> missing_num([0])
+    1
+    >>> missing_num([1, 2])
+    0
+    >>> missing_num([1])
+    0
+    """
+    if len(nums) == 1:
+        if nums[0] == 0:
+            return 1
+        else:
+            return 0
+    nums.sort()
+    if nums[0] != 0:
+        return 0
+    i = 0
+    while i < len(nums) - 1:
+        if nums[i] + 1 != nums[i + 1]:
+            return nums[i] + 1
+        i += 1
+    return nums[-1] + 1
+
+    # pseudocode:
+    # if length of list is 1, return 1
+    # sort list
+    # index variable
+    # loop through list, to second to last spot 
+    # compare number to next number, if not + 1, return current num + 1
+    # after loop return nums[-1] + 1
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
